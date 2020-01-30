b0d12b90b092733412d050ae51bb062ed3eab52f
santosh1817 <santosh01994@gmail.com>
2020-01-30 08:12:48 +0530

    santosh added app bar in app.js material ui

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,30 @@ import List from './components/List'
 import Add from './components/Addcontact'
 import ContactShow from './components/Show'
 
-class App extends React.Component{
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+//import MenuIcon from '@material-ui/icons/Menu';
+
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+
+const  App=(props)=>{
   
 //   componentDidMount()
 //   {
@@ -18,16 +41,24 @@ class App extends React.Component{
 //           contacts:response.data,
 
 //         }))
-//       })
 //   }
-  render()
-  {
+//       })
+//  render()
+//  {
+
+  const classes = useStyles();
     return(
-        <div>
+        <div class={classes.root}>
+        <AppBar position="static">
+        <Toolbar>
+        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">  
+        {/* <MenuIcon/> */}
+        </IconButton>
+
         <BrowserRouter>
          
-        <h1>Conatact manager</h1>
-        <Link to="/"> Home </Link> |
+        <h1>Contact manager</h1><br/>
+        <Link to="/"> Home </Link> 
         <Link to="/contacts">Contacts</Link>
 
         
@@ -38,9 +69,12 @@ class App extends React.Component{
             <Route path="/contacts/:id" component={ContactShow}/>
             </Switch>
             </BrowserRouter>
+         
+          </Toolbar>  
+          </AppBar>  
       </div>
     )
-  }
+//  }
 }
 
 export default App
